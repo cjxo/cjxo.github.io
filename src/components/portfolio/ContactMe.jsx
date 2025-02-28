2999f3dfb6db686cea527d41f02c5d4c817d3988
Christian <64344406+cjxo@users.noreply.github.com>
2025-02-28 12:00:35 +0800

    Working contact me

--- src/components/portfolio/ContactMe.jsx
+++ src/components/portfolio/ContactMe.jsx
@@ -4,12 +4,14 @@ const ContactMe = () => {
   return (
     <section className={styles.category} id="contact-me">
       <h2 className={styles.title}>Contact Me</h2>
-      <form className={styles.contactMeForm}>
+      <form action="https://api.web3forms.com/submit" method="POST" className={styles.contactMeForm}>
+        <input type="hidden" name="access_key" value="4c9c7686-1395-424b-aa72-3d94477e584b" />
         <input type="text" name="name" placeholder="Your Name" required />
         <input type="email" name="email" placeholder="Your Email" required />
         <input type="text" name="subject" placeholder="Subject" required />
         <textarea name="message" rows="6" placeholder="Your Message" required></textarea>
-        <button className={styles.buttonLike}>Submit Message</button>
+
+        <button type="submit" className={styles.buttonLike}>Submit Message</button>
       </form>
     </section>
   );
